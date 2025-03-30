7409caf072979c85343fe693285159f7c110d29d
Edward Tirado Jr <ed@etdevelopment.net>
2025-03-30 00:57:14 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+# Stage 1: Base build stage
+FROM python:3.13-slim AS builder
+
+RUN mkdir /app
+
+WORKDIR /app
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+RUN pip install --upgrade pip
+
+COPY requirements.txt /app/
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Stage 2: Production stage
+FROM python:3.13-slim
+
+RUN useradd -m -r web && \
+    mkdir /app && \
+    chown -R web /app
+
+COPY --from=builder /usr/local/lib/python3.13/site-packages/ /usr/local/lib/python3.13/site-packages/
+COPY --from=builder /usr/local/bin/ /usr/local/bin/
+
+WORKDIR /app
+
+COPY --chown=web:web . .
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+USER web
+
+EXPOSE 8000
+
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "--workers", "3", "djangodocker.wsgi:application"]
---
+++ users/views.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import login
+from django.contrib.auth.models import Group, User, AnonymousUser
+from rest_framework import permissions, viewsets, status
+from knox.auth import TokenAuthentication
+from knox.views import LoginView as KnoxLoginView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from users.serializers import GroupSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+
+
+@api_view(["POST"])
+def register(request):
+    user_data = UserSerializer(data=request.data)
+
+    if user_data.is_valid():
+        User.objects.create_user(**user_data.validated_data)
+        return Response(request.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response([], status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(KnoxLoginView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        login(request, user)
+        return super(LoginView, self).post(request, format=None)
