a58198b5c93a3f769f991d72a3843fa5998a4014
Edward Tirado Jr <ed@etdevelopment.net>
2025-04-13 01:49:01 -0500

    removed debug code

--- movie_db/db_providers/omdb.py
+++ movie_db/db_providers/omdb.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 from movie_db.movie_db import MovieDB
@@ -10,7 +9,6 @@ from movie_db.serializers import MovieSerializer, MovieResultSerializer
 class OMDb(MovieDB):
     def __init__(self):
         api_key = os.getenv("OMDB_API_KEY")
-        logging.log(1, f"WTF: {api_key}")
         self.api_key = f"{api_key}"
         self.base_url = "https://www.omdbapi.com/?apikey=" + self.api_key
         super().__init__()
