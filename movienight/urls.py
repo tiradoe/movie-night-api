93d6c6899c58b8511f9e38408c1acb76864fd9a1
Edward Tirado Jr <ed@etdevelopment.net>
2025-04-22 01:16:42 -0500

    added api versioning

--- movienight/urls.py
+++ movienight/urls.py
@@ -16,19 +16,19 @@ from movie_db import views as movie_db_views
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
-router.register(r"api/users", user_views.UserViewSet)
-router.register(r"api/groups", user_views.GroupViewSet)
-router.register(r"api/movies", movie_views.MovieViewset)
-router.register(r"api/lists", movie_views.MovieListViewset)
-router.register(r"api/schedules", movie_views.ScheduleViewset)
-router.register(r"api/showings", movie_views.ShowingViewset)
+router.register(r"v1/users", user_views.UserViewSet)
+router.register(r"v1/groups", user_views.GroupViewSet)
+router.register(r"v1/movies", movie_views.MovieViewset)
+router.register(r"v1/lists", movie_views.MovieListViewset)
+router.register(r"v1/schedules", movie_views.ScheduleViewset)
+router.register(r"v1/showings", movie_views.ShowingViewset)
 
 urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
-    path(r"api/auth/token/", obtain_auth_token),
-    path(r"api/auth/login/", user_views.LoginView.as_view(), name="knox_login"),
-    path(r"api/auth/register/", user_views.register, name="register"),
-    path(r"api/movies/search", movie_db_views.omdb_search, name="omdb_search"),
-    path(r"api/auth/", include("knox.urls")),
+    path(r"v1/auth/token/", obtain_auth_token),
+    path(r"v1/auth/login/", user_views.LoginView.as_view(), name="knox_login"),
+    path(r"v1/auth/register/", user_views.register, name="register"),
+    path(r"v1/movies/search", movie_db_views.omdb_search, name="omdb_search"),
+    path(r"v1/auth/", include("knox.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
